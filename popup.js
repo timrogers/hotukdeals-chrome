dd0574b217991459ce621c2887c4a85f8588a3de
Tim Rogers <tim@tim-rogers.co.uk>
2010-08-30 11:14:03 +0100

    Updated popup.js so that you can also click the deal title to view the full details. Bug was noticed during testing prior to build of v1.0 - this change will be included in the final build.

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function() {
 		var dealNumber = Math.floor(Math.random()*101);
 		jQuery("#jqt").append("<div id='" + dealNumber + "'><div class='toolbar'><a href='#home' class='button back backButton'>Back</a><h1>View Deal<h1></div><ul><li>" + item.title.substr(0,55).replace("Â", "") + "...</li><li style='font-weight: normal;'>" + item.merchant.name + "<small class='counter'>" + Math.round(item.temperature) + "</small></li></ul><ul class='rounded'><li style='font-weight: normal;'>" + item.description.substr(0,300).replace("Â", "") + "...</li></ul><ul class='rounded'><li class='arrow'><a href='#' onclick='newTab(\"" + item.deal_link + "\");'>View on HotUKDeals</a></li></ul></div>");
 		
-		jQuery("div#home").append("<ul><li>" + item.title.substr(0,55).replace("Â", "") + "...</li><li style='font-weight: normal;' class='arrow'><a href='#" + dealNumber + "'>" + item.merchant.name + "<small class='counter'>" + Math.round(item.temperature) + "</small></a></li></ul>");
+		jQuery("div#home").append("<ul><li><a href='#" + dealNumber + "'>" + item.title.substr(0,55).replace("Â", "") + "...</a></li><li style='font-weight: normal;' class='arrow'><a href='#" + dealNumber + "'>" + item.merchant.name + "<small class='counter'>" + Math.round(item.temperature) + "</small></a></li></ul>");
 	});
 	});
 });
